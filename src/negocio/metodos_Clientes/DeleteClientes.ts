c02a61eb04e70f6c190b14b5963c48b6578b55f2
Vitória Moura <vim.saturnino@gmail.com>
2022-06-24 17:33:16 -0300

    erro de typo corrigido em DeleteClientes.ts

--- src/negocio/metodos_Clientes/DeleteClientes.ts
+++ src/negocio/metodos_Clientes/DeleteClientes.ts
@@ -7,23 +7,23 @@ export default class DeleteCliente extends Delete {
   private clientes: Array <Cliente>
 
   constructor( clientes:Array <Cliente> ){
-      super()
-      this.clientes = clientes
+      super();
+      this.clientes = clientes;
   }
 
   listaClientes(){
     let listaCLientes = new ListagemClientes(this.clientes);
-    listaCLientes.listar()
-    let entrada = new Entrada()
+    listaCLientes.listar();
+    let entrada = new Entrada();
 
-    let cpf = entrada.receberTexto("Informe o CPF do cliente que deseja deletar: ")
-        let indice = this.clientes.findIndex( i => i.getCpf.getValor === cpf)
-        this.clientes.splice(indice, 1)
-        return
+    let cpf = entrada.receberTexto("Informe o CPF do cliente que deseja deletar: ");
+        let indice = this.clientes.findIndex( i => i.getCpf.getValor === cpf);
+        this.clientes.splice(indice, 1);
+        return;
   }
 
   public delete(): void {
-    this.listaClientes()
-    console.log('Cliente foi excluido com sucesso!')
+    this.listaClientes();
+    console.log("Cliente foi excluido com sucesso!");
 }
 }

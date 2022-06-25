1a70cef5c7e1a2955b0a6ad22babf7332029dd1d
Vitória Moura <vim.saturnino@gmail.com>
2022-06-24 22:58:59 -0300

    menu da main atualizado

--- src/app/main.ts
+++ src/app/main.ts
@@ -1,35 +1,56 @@
 import Entrada from "../io/entrada";
 import Empresa from "../modelo/empresa"
-import CadastroCliente from "../negocio/metodos_Clientes/CadastroCliente";
-import ListagemClientes from "../negocio/metodos_Clientes/ListagemClientes";
+import SwitchCaseCliente from "./switchCases/clientes";
+import SwitchCaseProdutos from "./switchCases/produtos";
+import SwitchCaseServicos from "./switchCases/servicos";
+import SwitchCaseListasAtvi from "./switchCases/listasAtvi";
+import MockClientes from "../mock/clientes";
 
-console.log(`Bem-vindo ao cadastro de clientes do Grupo World Beauty`)
+console.log("Bem-vindo ao cadastro de clientes do Grupo World Beauty")
 let empresa = new Empresa()
 let execucao = true
 
+// Lista de clientes para teste
+// let mockClientes = new MockClientes(empresa.getClientes)
+// mockClientes.cadastrar();
+
 while (execucao) {
-    console.log(`Opções:`);
-    console.log(`1 - Cadastrar cliente`);
-    console.log(`2 - Listar todos os clientes`);
-    console.log(`0 - Sair`);
+    console.log("Opções:");
+    console.log("1 - Cadastrar cliente");
+    console.log("2 - Produtos");
+    console.log("3 - Serviços");
+    console.log("4 - Listas (atvi)");
+    console.log("0 - Sair");
 
     let entrada = new Entrada()
-    let opcao = entrada.receberNumero(`Por favor, escolha uma opção: `)
+    let opcao = entrada.receberNumero("Por favor, escolha uma opção: ")
 
     switch (opcao) {
         case 1:
-            let cadastro = new CadastroCliente(empresa.getClientes)
-            cadastro.cadastrar()
+            let switch_case_cliente = new SwitchCaseCliente(empresa)
+            switch_case_cliente.listar()
             break;
+
         case 2:
-            let listagem = new ListagemClientes(empresa.getClientes)
-            listagem.listar()
+            let switch_case_produtos = new SwitchCaseProdutos(empresa)
+            switch_case_produtos.listar()
             break;
+
+        case 3:
+            let switch_case_servicos = new SwitchCaseServicos(empresa)
+            switch_case_servicos.listar()
+            break;
+
+        case 4:
+            let switch_case_listas_atvi = new SwitchCaseListasAtvi(empresa)
+            switch_case_listas_atvi.listar()
+            break;
+
         case 0:
             execucao = false
-            console.log(`Até mais`)
+            console.log("Saindo")
             break;
         default:
-            console.log(`Operação não entendida :(`)
+            console.log("Insira uma opção válida")
     }
 }
